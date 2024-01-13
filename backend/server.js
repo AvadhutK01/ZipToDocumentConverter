a18789cfed9db3e1311fccb3d2891c0fb50145e8
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-13 23:22:26 +0530

    preparation for vercel

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter)
 app.use('/zip', zipRouter)
 app.use('/pdf', pdfRouter);
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
