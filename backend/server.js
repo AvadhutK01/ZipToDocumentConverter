31194a7f21d4e93fa0890ea91cdff0d7b80323fc
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-13 23:26:32 +0530

    preparation stage 2

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const zipRouter = require('./Routes/ZipRoute');
 const userRouter = require('./Routes/userRoute');
 require('./ConnectDb');
 const pdfRouter = require('./Routes/pdfRoute');
+const mongoose = require("mongoose");
 const app = express();
 app.use(cors({
     origin: '*',
@@ -19,6 +20,11 @@ app.use('/zip', zipRouter)
 app.use('/pdf', pdfRouter);
 app.get('/', (req, res) => {
     res.send('Hello');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to MongoDB:', err);
+    // Send the error response to the client
+    res.status(500).send('Error connecting to MongoDB: ' + err.message);
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
